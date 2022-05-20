05b20d1b0f6e6eb5e3c7aead6ad139330f481793
skad12 <stephenkunle26@gmail.com>
2022-05-20 19:43:52 +0100

    initial

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ function weather() {
 </Head>
 
 
-<div className="grid min-h-screen antialiased text-white bg-gray-400 place-items-center">
+<div className="grid min-h-screen antialiased bg-cover text-white place-items-center" style={{ backgroundImage: `url('https://images.pexels.com/photos/1671324/pexels-photo-1671324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`}}>
 
   {/* box content */}
   <div className="box-content grid w-2/6 grid-cols-2 bg-black rounded-md h-72">
-    <div className="p-4 space-y-4 bg-cover rounded-md" style={{ backgroundImage: `url('https://images.pexels.com/photos/3825179/pexels-photo-3825179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')` }} >
+    <div className="p-4 space-y-4 bg-cover rounded-md" style={{ backgroundImage: `url('https://images.pexels.com/photos/3825179/pexels-photo-3825179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')`}}>
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -54,3 +54,8 @@ function weather() {
 }
 
 export default weather;
+
+
+
+
+
